eb388704f47dfa43fb4db30b79acd9d007568200
rabbitg <443504635@qq.com>
2019-11-18 15:35:04 +0800

    update

---
+++ demo/RegExp/read-正则必知必会.js
@@ -0,0 +1,6 @@
+// 1. 匹配纯文本
+
+// Ben
+var str = "abc123def";
+var patt1 = /[0-9]+/;
+console.log(str.match(patt1));
---
+++ demo/eslint demo/a.js
@@ -0,0 +1,8 @@
+var a = 'abg'
+if (a) {
+	return '1'
+}
+var a = ['1', '2']
+async addOrModify({ state, commit}) {
+
+}
